8a62dc7016598fb966b69b0cb9c9e26e1b0d8e40
Fábio Moreira <fabiomoreirafms@gmail.com>
2018-04-27 13:31:14 -0300

    Alteração em Exercício de Thread

--- ExercicioThread.py
+++ ExercicioThread.py
@@ -2,45 +2,52 @@ import threading
 import time
 import random
 
-class market(object):
+class queue(object):
     lock = threading.RLock()
-
     def __init__(self):
-        self.total_items = 0
+        self.item = -1
 
-    def add(self,n):
+    def add(self, n):
         self.lock.acquire()
-        self.total_items = n
+        self.item = n
         self.lock.release()
 
     def remove(self):
         self.lock.acquire()
-        saida = self.total_items
+        saida = self.item
+        self.item = -1
         self.lock.release()
         return saida
 
 
-def producer(box, inputs, index):
+def producer(queue, inputs, index):
     while inputs > 0:
         item = random.randint(0,256)
         print("Producer notify: item N{0} adicionado por {1}".format(item, index))
-        box.add(item)
+        queue.add(item)
         time.sleep(1)
         inputs -= 1
 
-def consumer(box, outputs,index):
-    if outputs > 0:
-        item = box.remove()
-        print ("Consumer notify: {0} retirado por {1}".format(item, index))
-        time.sleep(1)
-        outputs-=1
+def consumer(queue, index):
+    global inputs
+    while inputs > 0:
+        item = queue.remove()
+        if item>=0:
+            print ("Consumer notify: {0} retirado por {1}".format(item, index))
+        inputs-=1
 
 if __name__=="__main__":
     inputs = 10
-    box = market()
-    t1 = threading.Thread(target=producer, args = (box,inputs, 1))
-    t2 = threading.Thread(target=consumer, args=(box, inputs,2))
+    queue = queue()
+    t1 = threading.Thread(target=producer, args = (queue,inputs, 1))
+    t2 = threading.Thread(target=consumer, args=(queue, 2))
+    t3 = threading.Thread(target=consumer, args=(queue, 3))
+    t4 = threading.Thread(target=consumer, args=(queue, 4))
     t1.start()
     t2.start()
+    t3.start()
+    t4.start()
     t1.join()
     t2.join()
+    t3.join()
+    t4.join()
